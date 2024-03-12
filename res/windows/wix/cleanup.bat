e121e5750ae674e7d0697608c6aaa04c2f8e6ced
unknown <roman@romanlab.com>
2024-03-12 04:19:26 +0300

    Update cleanup.bat

--- res/windows/wix/cleanup.bat
+++ res/windows/wix/cleanup.bat
@@ -1,1 +1,6 @@
-rd /Q /S "%USERPROFILE%\AppData\Local\space-acres"
+@echo off
+:UseChoice
+%SystemRoot%\System32\choice.exe /C YN /N /M "Delele configuration and logs [Y/N]?"
+if not errorlevel 1 goto UseChoice
+if errorlevel 2 exit /B
+for /F "delims=" %%I in ('dir "%HOMEDRIVE%\Users\*.*" /AD /B 2^>nul') do rd /Q /S "%HOMEDRIVE%\Users\%%I\AppData\Local\space-acres" 2>nul
