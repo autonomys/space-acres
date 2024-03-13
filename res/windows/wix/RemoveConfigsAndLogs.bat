e60cc84d37e24deb031111c1235fb1ecdf1bc540
RomanLabGit <113828852+RomanLabGit@users.noreply.github.com>
2024-03-13 17:08:45 +0300

    Update res/windows/wix/RemoveConfigsAndLogs.bat
    Co-authored-by: Nazar Mokrynskyi <nazar@mokrynskyi.com>

--- res/windows/wix/RemoveConfigsAndLogs.bat
+++ res/windows/wix/RemoveConfigsAndLogs.bat
@@ -1,6 +1,6 @@
 @echo off
 :UseChoice
-%SystemRoot%\System32\choice.exe /C YN /N /M "Delele configuration and logs [Y/N]?"
+%SystemRoot%\System32\choice.exe /C YN /N /M "Delete Space Acres configuration and logs for all users [Y/N]?"
 if not errorlevel 1 goto UseChoice
 if errorlevel 2 exit /B
 for /F "delims=" %%I in ('dir "%HOMEDRIVE%\Users\*.*" /AD /B 2^>nul') do rd /Q /S "%HOMEDRIVE%\Users\%%I\AppData\Local\space-acres" 2>nul
