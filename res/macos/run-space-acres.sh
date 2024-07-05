86cad862d30e7eee53d95ec7037b850c97d83985
Michael Lip <lipmichal@gmail.com>
2024-07-05 18:43:41 +0700

    Install svg loaders and schema

--- res/macos/run-space-acres.sh
+++ res/macos/run-space-acres.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
-EXECUTABLE_PATH="$(pwd)"
 
-export GDK_PIXBUF_MODULE_FILE="$EXECUTABLE_PATH/../Resources/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache"
-
-exec "$EXECUTABLE_PATH/space-acres"
+export GSETTINGS_SCHEMA_DIR="/Applications/SpaceAcres.app/Contents/Resources/share/glib-2.0/schemas"
+export GDK_PIXBUF_MODULE_FILE="/Applications/SpaceAcres.app/Contents/Resources/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache"
+./space-acres
