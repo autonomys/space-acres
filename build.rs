012ad263c7dd2353a1aabb7c849895af96cae907
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2024-08-03 06:03:43 +0300

    Fix some translation errors

--- build.rs
+++ build.rs
@@ -5,10 +5,22 @@ use std::{env, fs};
 fn main() {
     // TODO: Workaround for https://github.com/zaytsev/fluent-static/issues/4
     println!("cargo:rerun-if-changed=res/translations");
-
+    let mut generated =
+        generate("res/translations", MessageBundleCodeGenerator::new("en")).unwrap();
+    // This is a hack for making sure `NUMBER()` is supported, see https://github.com/projectfluent/fluent-rs/pull/353#issuecomment-2266336661
+    {
+        if !generated.contains("; bundle }") {
+            panic!("Unexpected generated contents: {generated}");
+        }
+        generated = generated.replace(
+            "; bundle }",
+            // TODO: Should have been `bundle.add_builtins().unwrap();`, but https://github.com/projectfluent/fluent-rs/issues/368
+            r#"; bundle.add_function("NUMBER", super::number).unwrap(); bundle }"#,
+        );
+    }
     fs::write(
         Path::new(&env::var("OUT_DIR").unwrap()).join("l10n.rs"),
-        generate("res/translations", MessageBundleCodeGenerator::new("en")).unwrap(),
+        generated,
     )
     .unwrap();
 
